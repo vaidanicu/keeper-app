6bad9a5465c8be65b56ad8e6c1bc2e0bad1bd6fd
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-06-09 11:22:13 +0300

    //5. Create a Note.jsx component to show a <div> element with a //<h1> for a title and a <p> for the content.

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Note } from "./Note";
 
 export function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
---
+++ src/component/Note.jsx
@@ -0,0 +1,8 @@
+export function Note() {
+  return (
+    <div className="note">
+      <h1>This is the note title</h1>
+      <p>This is the note content</p>
+    </div>
+  );
+}
