1f99a94686ac5a2a44b283281a2584e65f9b6c4a
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-13 13:54:57 +0300

    Pass the new note back to the App

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -4,10 +4,14 @@ import { Note } from "./Note";
 
 import CreateArea from "./CreateArea";
 export function App() {
+  function addNote(note) {
+    console.log(note);
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 import { App } from "./component/App";
 
-ReactDom.render(
-  <>
-    <App />
-  </>,
-  document.getElementById("root")
-);
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<App />);
